dae68fc60c2fbbd8d66b70d24b06e16fe6e51066
student-PiyushMishra <student.piyushmishra@gmail.com>
2025-02-14 22:54:34 +0530

    added content for about and contact page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import Users from './pages/Users';  
 import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/users" element={<Users />} />
       </Routes>
     </>
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='text-white'>Namste from contact page</div>
+  )
+}
+
+export default Contact

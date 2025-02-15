e123cb5d7817a18fcfc2383d8e94873ad29ca4c2
student-PiyushMishra <student.piyushmishra@gmail.com>
2025-02-15 18:57:30 +0530

    added the musci page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
 
 const Contact = () => {
   return (
-    <div className='text-white'>Namste from contact page</div>
-  )
-}
+    <div className="text-white p-6 w-full ">
+      <iframe
+        className='mb-10'
+        style={{ borderRadius: '12px' }}
+        src="https://open.spotify.com/embed/track/3h5YZTfJ4BBpvLVHS6lGUl?utm_source=generator"
+        width="100%"
+        height="352"
+        frameBorder="0"
+        allowFullScreen
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
+      <iframe
+        className='mb-10'
+        style={{ borderRadius: '12px' }}
+        src="https://open.spotify.com/embed/track/0mtS8143MsPJfSM9fvFUhO?utm_source=generator"
+        width="100%"
+        height="352"
+        frameBorder="0"
+        allowFullScreen
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
+      <iframe
+        style={{ borderRadius: '12px' }}
+        src="https://open.spotify.com/embed/track/0XprgFKfXIDkgHZ4iYVIjk?utm_source=generator"
+        width="100%"
+        height="352"
+        frameBorder="0"
+        allowFullScreen
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
+      
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
